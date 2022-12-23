8f2b9f60f3eb8a276c9874936ad1c749c0e6e2fd
Kevin Mak <kmak1129@gmail.com>
2022-12-23 11:46:50 -0500

    failed

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import PokemonPage from "./PokemonPage";
+import PokemonCard from "./PokemonCard";
+import {useState} from "react"
 
 function App() {
+  const [pokemon,setPokemon] = useState([])
   return (
     <div className="App">
       <PokemonPage />
+      <PokemonCard 
+      pokemon={pokemon} 
+      setPokemon={setPokemon}
+      />
     </div>
   );
 }
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,20 +1,37 @@
 import React from "react";
 import { Card } from "semantic-ui-react";
+import {useEffect} from "react"
 
-function PokemonCard() {
+
+function PokemonCard({pokemon,setPokemon}) {
+  
+  const getPokemons = async () => {
+    const request = await fetch (" http://localhost:3000/pokemon")
+    const response = await request.json()
+    setPokemon(response)
+  }
+
+  useEffect(() => {
+    getPokemons();
+  }, [])
+
   return (
+  
     <Card>
       <div>
         <div className="image">
           <img alt="oh no!" />
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{pokemons.map((pokemon)=>{
+            return(key={pokemon.name})
+          }
+          </div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            {pokemon.hp}
           </span>
         </div>
       </div>
